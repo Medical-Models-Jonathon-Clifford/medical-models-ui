f8cc89b06dac2edf9abd63a3e2b578491d3c5660
JonathonClifford <jono.cliff@gmail.com>
2025-02-05 12:07:38 +0000

    test: MM-39 Add initial tests for the comments.ts file. Start a data generator. (#33)
    Adding initial tests for the comments.ts file.
    
    Started a data-generator.ts file for generating data for tests. Used the builder pattern here.
    
    Reviewed-on: http://gitea.busybunyip.com/medical-models/medical-models-ui/pulls/33
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- apps/medical-models/features/comments/utils/comments.spec.ts
+++ apps/medical-models/features/comments/utils/comments.spec.ts
@@ -1,24 +1,79 @@
-import { CommentNode, countComments } from './comments';
+import {
+  CommentNode,
+  compareComments,
+  countComments,
+  deleteCommentNode,
+} from './comments';
+import {
+  CommentNodeBuilder,
+  newTestCommentNode,
+} from '../../../testing/data-generator';
 
 describe('Comment Utils', () => {
   describe('countComments', () => {
-    it('should render successfully', () => {
-      const testCommentNodes: CommentNode[] = [
-        {
-          comment: {
-            id: '1',
-            document_id: '10',
-            body: 'Test comment',
-            createdDate: new Date(Date.parse('04 Mar 2010 00:12:00 GMT')),
-            modifiedDate: new Date(Date.parse('04 Mar 2010 00:12:02 GMT')),
-          },
-          children: [],
-        },
-      ];
+    it('should count a single comment as 1', () => {
+      const testCommentNodes: CommentNode[] = [newTestCommentNode()];
 
       const count = countComments(testCommentNodes);
 
       expect(count).toBe(1);
+    });
+  });
+
+  describe('compareComments', () => {
+    it('should return 0 when given the same comment twice', () => {
+      const testCommentNode: CommentNode = newTestCommentNode();
+
+      const count = compareComments(testCommentNode, testCommentNode);
+
+      expect(count).toBe(0);
+    });
+
+    it('should return 1 when given first comment is after the second', () => {
+      const testCommentNode1 = newTestCommentNode('04 Mar 2010 00:13:00 GMT');
+      const testCommentNode2 = newTestCommentNode('04 Mar 2010 00:12:00 GMT');
+
+      const count = compareComments(testCommentNode1, testCommentNode2);
+
+      expect(count).toBe(1);
+    });
+
+    it('should return -1 when given first comment is before the second', () => {
+      const testCommentNode1 = newTestCommentNode('04 Mar 2010 00:12:00 GMT');
+      const testCommentNode2 = newTestCommentNode('04 Mar 2010 00:13:00 GMT');
+
+      const count = compareComments(testCommentNode1, testCommentNode2);
+
+      expect(count).toBe(-1);
+    });
+  });
+
+  describe('deleteCommentNode', () => {
+    it('should delete single node in tree', () => {
+      const testCommentNode = newTestCommentNode();
+      const testCommentNodes: CommentNode[] = [testCommentNode];
+
+      const remainingNodes = deleteCommentNode(
+        testCommentNode,
+        testCommentNodes
+      );
+
+      expect(remainingNodes.length).toBe(0);
+    });
+    it('should delete leaf node in a path tree of depth 2', () => {
+      const childCommentNode = newTestCommentNode();
+      const parentCommentNode = new CommentNodeBuilder()
+        .id('2')
+        .children([childCommentNode])
+        .build();
+      const testCommentNodes: CommentNode[] = [parentCommentNode];
+
+      const remainingNodes = deleteCommentNode(
+        childCommentNode,
+        testCommentNodes
+      );
+
+      expect(remainingNodes.length).toBe(1);
     });
   });
 });
---
+++ apps/medical-models/testing/data-generator.ts
@@ -0,0 +1,61 @@
+import { CommentNode } from '../features/comments/utils/comments';
+import { addMinutes } from 'date-fns';
+
+export const newTestCommentNode = (
+  createdDateStr = '04 Mar 2010 00:12:00 GMT'
+): CommentNode => {
+  const createdDate: Date = new Date(Date.parse(createdDateStr));
+  const modifiedDate: Date = addMinutes(createdDate, 2);
+  return {
+    comment: {
+      id: '1',
+      document_id: '10',
+      body: 'Test comment',
+      createdDate: createdDate,
+      modifiedDate: modifiedDate,
+    },
+    children: [],
+  };
+};
+
+export class CommentNodeBuilder {
+  commentNode: CommentNode;
+
+  constructor() {
+    this.commentNode = newTestCommentNode();
+  }
+
+  id(id: string) {
+    this.commentNode.comment.id = id;
+    return this;
+  }
+
+  document_id(document_id: string) {
+    this.commentNode.comment.document_id = document_id;
+    return this;
+  }
+
+  body(body: string) {
+    this.commentNode.comment.body = body;
+    return this;
+  }
+
+  createdDate(createdDate: Date) {
+    this.commentNode.comment.createdDate = createdDate;
+    return this;
+  }
+
+  modifiedDate(modifiedDate: Date) {
+    this.commentNode.comment.modifiedDate = modifiedDate;
+    return this;
+  }
+
+  children(children: CommentNode[]) {
+    this.commentNode.children = children;
+    return this;
+  }
+
+  build(): CommentNode {
+    return this.commentNode;
+  }
+}
