76b675835956cb71e1ee1c333e9b5a4ff0b5020b
JonathonClifford <jono.cliff@gmail.com>
2025-02-09 09:39:31 +0000

    feat: MM-55 Add initial favicon (#44)
    Reviewed-on: http://gitea.busybunyip.com/medical-models/medical-models-ui/pulls/44
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- apps/medical-models/app/layout.tsx
+++ apps/medical-models/app/layout.tsx
@@ -13,6 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/favicon.png"
+          type="image/png"
+          sizes="<generated>"
+        />
+      </head>
       <body>
         <Base>{children}</Base>
       </body>
