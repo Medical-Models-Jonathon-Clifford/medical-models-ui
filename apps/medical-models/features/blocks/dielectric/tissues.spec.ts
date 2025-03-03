08fd5904e03af38764dfe8d867c34ce9d7187c00
JonathonClifford <jono.cliff@gmail.com>
2025-03-03 10:20:39 +0000

    test: MM-65 Adding prettier formatting to tissues.spec.ts. (#51)
    Reviewed-on: http://gitea.busybunyip.com/medical-models/medical-models-ui/pulls/51
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- apps/medical-models/features/blocks/dielectric/tissues.spec.ts
+++ apps/medical-models/features/blocks/dielectric/tissues.spec.ts
@@ -7,11 +7,14 @@ describe('tissues', () => {
     ${'Skin (Wet)'}     | ${79.577}
     ${'Stomach'}        | ${79.577}
     ${'Vitreous Humor'} | ${159.155}
-  `('should return the correct tissue for $tissueName', ({tissueName, expectedTau2}) => {
-    let actual = tissueFromName(tissueName);
-    expect(actual.name).toBe(tissueName);
-    expect(actual.tau2).toBe(expectedTau2);
-  });
+  `(
+    'should return the correct tissue for $tissueName',
+    ({ tissueName, expectedTau2 }) => {
+      const actual = tissueFromName(tissueName);
+      expect(actual.name).toBe(tissueName);
+      expect(actual.tau2).toBe(expectedTau2);
+    }
+  );
 
   it.each`
     tissueName
@@ -19,7 +22,7 @@ describe('tissues', () => {
     ${''}
     ${'Skin'}
     ${'Vitreous Humor Stomach'}
-  `('should throw an error for tissues that do not exist', ({tissueName}) => {
+  `('should throw an error for tissues that do not exist', ({ tissueName }) => {
     expect(() => tissueFromName(tissueName)).toThrow();
   });
 });
