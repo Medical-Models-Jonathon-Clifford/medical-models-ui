e6b48148e68eba611a5431f3d539a61f14c38671
JonathonClifford <jono.cliff@gmail.com>
2025-03-16 03:51:45 +0000

    feat: MM-69 Swapping to use the MUI Avatar component instead of my custom profile icon component. (#58)
    Reviewed-on: http://gitea.busybunyip.com/medical-models/medical-models-ui/pulls/58
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- apps/medical-models/components/profile-icon/ProfileIcon.tsx
+++ apps/medical-models/components/profile-icon/ProfileIcon.tsx
@@ -1,8 +1,41 @@
-import Image from 'next/image';
-import stubProfilePic from '../../public/stub-profile-img.png';
+import Avatar from '@mui/material/Avatar';
+import styles from './ProfileIcon.module.scss';
+
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string, size: number) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+      width: size,
+      height: size,
+    },
+    children: (
+      <span className={styles.avatar_initials}>{`${name.split(' ')[0][0]}${
+        name.split(' ')[1][0]
+      }`}</span>
+    ),
+  };
+}
 
 export function ProfileIcon({ size = 32 }: { size?: number }) {
-  return (
-    <Image src={stubProfilePic} alt="Profile Icon" width={size} height={size} />
-  );
+  return <Avatar {...stringAvatar('John Smith', size)}></Avatar>;
 }
