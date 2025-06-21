163e5522a2199587637b65bde0c1595e88377f6f
JonathonClifford <jono.cliff@gmail.com>
2025-06-21 03:46:06 +0000

    feat: MM-84 Add SupportBody and SupportMenu components. (#83)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-ui/pulls/83
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

---
+++ apps/medical-models/features/base/SupportBody.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import styles from './Base.module.scss';
+import MainStage from './MainStage';
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import { memo, useState } from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import SupportMenu from './SupportMenu';
+
+type SideBarState = 'expanded' | 'collapsed';
+
+const EXPANDED_SIDE_BAR_WIDTH = 340;
+const COLLAPSED_SIDE_BAR_WIDTH = 20;
+const TOGGLE_BUTTON_WIDTH = 24;
+
+export function SupportBody({ children }: { children: React.ReactNode }) {
+  const [drawerWidth, setDrawerWidth] = useState(EXPANDED_SIDE_BAR_WIDTH);
+  const [sideBarState, setSideBarState] = useState<SideBarState>('expanded');
+
+  const handleClickCollapse = () => {
+    setSideBarState('collapsed');
+    setDrawerWidth(COLLAPSED_SIDE_BAR_WIDTH);
+  };
+
+  const handleClickExpand = () => {
+    setSideBarState('expanded');
+    setDrawerWidth(EXPANDED_SIDE_BAR_WIDTH);
+  };
+
+  const createToggleButton = (icon: React.ReactNode) => {
+    return function ToggleButton({
+      title,
+      onClick,
+    }: {
+      title: string;
+      onClick: () => void;
+    }) {
+      return (
+        <Tooltip title={title} placement="right">
+          <ListItemButton
+            onClick={onClick}
+            className={styles.base_collapse_icon}
+            sx={{
+              left: drawerWidth - TOGGLE_BUTTON_WIDTH / 2,
+            }}
+          >
+            <ListItemIcon
+              sx={{ width: TOGGLE_BUTTON_WIDTH, minWidth: TOGGLE_BUTTON_WIDTH }}
+            >
+              {icon}
+            </ListItemIcon>
+          </ListItemButton>
+        </Tooltip>
+      );
+    };
+  };
+
+  const CollapseToggleButton = memo(createToggleButton(<ChevronLeftIcon />));
+  const ExpandToggleButton = memo(createToggleButton(<ChevronRightIcon />));
+
+  return (
+    <Box className={styles.base_drawer_and_main_stage}>
+      <SupportMenu
+        open={sideBarState === 'expanded'}
+        width={drawerWidth}
+      ></SupportMenu>
+      {sideBarState === 'expanded' && (
+        <CollapseToggleButton title="Collapse" onClick={handleClickCollapse} />
+      )}
+      {sideBarState === 'collapsed' && (
+        <ExpandToggleButton title="Expand" onClick={handleClickExpand} />
+      )}
+      <MainStage drawerWidth={drawerWidth}>{children}</MainStage>
+    </Box>
+  );
+}
---
+++ apps/medical-models/features/base/SupportMenu.tsx
@@ -0,0 +1,61 @@
+import List from '@mui/material/List';
+import React from 'react';
+import styles from './DrawerMenu.module.scss';
+import { NewDrawer } from '../../components/drawer/NewDrawer';
+import Link from 'next/link';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
+
+const INITIAL_BUTTON_PADDING = 8;
+const EXTRA_PADDING_PER_LEVEL = 15;
+
+export default function SupportMenu({
+  open,
+  width,
+}: {
+  open: boolean;
+  width: number;
+}) {
+  function getPaddingLeft() {
+    return `${INITIAL_BUTTON_PADDING + EXTRA_PADDING_PER_LEVEL}px`;
+  }
+
+  const DocItemButton = ({
+    children,
+    href,
+  }: {
+    children: React.ReactNode;
+    href: string;
+  }) => {
+    return (
+      <Button
+        className={styles.nav_tree_doc_item_button}
+        sx={{
+          paddingLeft: getPaddingLeft(),
+          backgroundColor: 'rgba(25, 118, 210, 0.1)',
+          ':hover': {
+            backgroundColor: 'rgba(25, 118, 210, 0.2)',
+          },
+        }}
+        href={href}
+      >
+        {children}
+      </Button>
+    );
+  };
+
+  return (
+    <NewDrawer width={width}>
+      {open && (
+        <List component="nav">
+          <Stack direction="column">
+            <Link href="/">Home</Link>
+            <DocItemButton href="/">Support Dashboard</DocItemButton>
+            <DocItemButton href="/organisations">Organisations</DocItemButton>
+            <DocItemButton href="/users">Users</DocItemButton>
+          </Stack>
+        </List>
+      )}
+    </NewDrawer>
+  );
+}
