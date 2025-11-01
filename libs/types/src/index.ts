a8d43c5da7607fcfbb545807b0c9052fea84c6cc
JonathonClifford <jono.cliff@gmail.com>
2025-11-01 11:50:40 +0000

    fix: MM-102 Fix auth export. (#154)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-ui/pulls/154
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,7 +1,7 @@
+export * from './lib/auth';
 export * from './lib/block';
 export * from './lib/comment';
 export * from './lib/dashboard';
 export * from './lib/document';
 export * from './lib/states';
 export * from './lib/user';
-export { AuthUser } from './lib/auth';
